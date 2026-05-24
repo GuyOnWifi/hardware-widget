e7320f37299f39a2c40dd5b20eb5a41af2f2ee3c
Eason Huang <me@easonhuang.dev>
2026-05-24 16:14:32 -0400

    feat: added GMCS pause/play next/back support + display

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,24 +7,35 @@
 )]
 #![deny(clippy::large_stack_frames)]
 
+use alloc::format;
+
 use bt_hci::{
     controller::ExternalController,
-    uuid::{characteristic::MEDIA_CONTROL_POINT, service::GENERIC_MEDIA_CONTROL},
+    uuid::{
+        characteristic::{
+            MEDIA_CONTROL_POINT, MEDIA_STATE, TRACK_DURATION, TRACK_POSITION, TRACK_TITLE,
+        },
+        service::GENERIC_MEDIA_CONTROL,
+    },
 };
 use defmt::{Debug2Format, error, info, unwrap};
 use embassy_executor::Spawner;
 use embassy_futures::{join::join, select::select};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
 use embassy_time::Timer;
 use embedded_graphics::{
     image::{Image, ImageRaw},
     mono_font::{MonoTextStyle, ascii::FONT_6X10},
     pixelcolor::BinaryColor,
     prelude::*,
+    primitives::{PrimitiveStyleBuilder, Rectangle},
     text::{Alignment, Text},
 };
 use embedded_storage::nor_flash::{NorFlash, ReadNorFlash};
 use esp_hal::{
+    Async,
     clock::CpuClock,
+    gpio::{Input, InputConfig, Pull},
     i2c::master::{Config, I2c},
     rng::{Trng, TrngSource},
     time::Rate,
@@ -33,18 +44,18 @@ use esp_hal::{
 use esp_radio::ble::controller::BleConnector;
 use esp_storage::FlashStorage;
 use panic_rtt_target as _;
-use rtt_target::{ChannelMode, DownChannel, set_defmt_channel, set_print_channel};
+use rtt_target::rtt_init_defmt;
 use sequential_storage::{
     cache::{KeyCacheImpl, NoCache},
     map::{Key, MapConfig, MapStorage, SerializationError, Value},
 };
-use ssd1306::{I2CDisplayInterface, Ssd1306Async, prelude::*};
+use ssd1306::{I2CDisplayInterface, Ssd1306Async, mode::BufferedGraphicsModeAsync, prelude::*};
 use trouble_host::prelude::*;
 
 extern crate alloc;
 
 const CONNECTIONS_MAX: usize = 1;
-const L2CAP_CHANNELS_MAX: usize = 1;
+const L2CAP_CHANNELS_MAX: usize = 3;
 
 const SCREEN_WIDTH: i32 = 128;
 const SCREEN_HEIGHT: i32 = 64;
@@ -181,11 +192,100 @@ async fn gatt_client_task<T: Controller, P: PacketPool, const M: usize>(
     }
 }
 
-#[allow(clippy::large_stack_frames, reason = "This is a big function")]
-async fn app_logic<T: Controller, P: PacketPool, const M: usize, D: DrawTarget>(
-    input: &mut DownChannel,
+async fn draw_screen(
+    song_title: &str,
+    percentage: f32,
+    playing: bool,
+    display: &mut Ssd1306Async<
+        I2CInterface<I2c<'_, Async>>,
+        DisplaySize128x64,
+        BufferedGraphicsModeAsync<DisplaySize128x64>,
+    >,
+) {
+    display.clear(BinaryColor::Off).unwrap();
+
+    let text_style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On);
+
+    let display_text = if song_title.len() > 15 {
+        format!("{}...", &song_title[..12])
+    } else {
+        song_title.into()
+    };
+
+    Text::with_alignment(
+        &display_text,
+        Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4),
+        text_style,
+        Alignment::Center,
+    )
+    .draw(display)
+    .unwrap();
+
+    // TODO: Rounded rectangles
+    let width = (SCREEN_WIDTH as f32 * 0.8) as i32;
+    let done = (percentage * width as f32) as i32;
+    let margin = (SCREEN_WIDTH as f32 * 0.1) as i32;
+
+    Rectangle::new(
+        Point::new(margin, SCREEN_HEIGHT / 2),
+        Size::new(done as u32, 4),
+    )
+    .into_styled(
+        PrimitiveStyleBuilder::new()
+            .stroke_color(BinaryColor::On)
+            .stroke_width(1)
+            .fill_color(BinaryColor::On)
+            .build(),
+    )
+    .draw(display)
+    .unwrap();
+
+    Rectangle::new(
+        Point::new(margin + done, SCREEN_HEIGHT / 2),
+        Size::new((width - done) as u32, 4),
+    )
+    .into_styled(
+        PrimitiveStyleBuilder::new()
+            .stroke_color(BinaryColor::On)
+            .stroke_width(1)
+            .fill_color(BinaryColor::Off)
+            .build(),
+    )
+    .draw(display)
+    .unwrap();
+
+    Image::with_center(
+        if playing { &PAUSE_IMG } else { &PLAY_IMG },
+        Point::new(SCREEN_WIDTH / 2, 3 * SCREEN_HEIGHT / 4),
+    )
+    .draw(display)
+    .unwrap();
+
+    Image::with_center(
+        &BACKWARD_IMG,
+        Point::new(SCREEN_WIDTH / 4, 3 * SCREEN_HEIGHT / 4),
+    )
+    .draw(display)
+    .unwrap();
+
+    Image::with_center(
+        &FORWARD_IMG,
+        Point::new(3 * SCREEN_WIDTH / 4, 3 * SCREEN_HEIGHT / 4),
+    )
+    .draw(display)
+    .unwrap();
+
+    display.flush().await.unwrap();
+}
+
+async fn app_logic<T: Controller, P: PacketPool, const M: usize>(
+    button_matrix: (&Input<'_>, &Input<'_>, &Input<'_>),
     client: &GattClient<'_, T, P, M>,
-    display: &mut D,
+    display: &mut Ssd1306Async<
+        I2CInterface<I2c<'_, Async>>,
+        DisplaySize128x64,
+        BufferedGraphicsModeAsync<DisplaySize128x64>,
+    >,
 ) {
     let services = client
         .services_by_uuid(&GENERIC_MEDIA_CONTROL.into())
@@ -195,53 +295,134 @@ async fn app_logic<T: Controller, P: PacketPool, const M: usize, D: DrawTarget>(
     let service = services.first().unwrap().clone();
     info!("Fetched service!");
 
-    loop {
-        let mut buf = [0u8; 64];
-        let count = input.read(&mut buf);
-        let cmd = core::str::from_utf8(&buf[..count]).unwrap_or("").trim();
+    let mcp_chr = client
+        .characteristic_by_uuid::<u8>(&service, &MEDIA_CONTROL_POINT.into())
+        .await
+        .unwrap();
+    info!("Fetched charactertistic");
 
-        match cmd {
-            "help" => {
-                info!(
-                    r#"
-                                        title - what is playing rn 
-                                        play  - play media 
-                                        pause - pause media 
-                                        "#
-                )
-            }
-            "title" => {
-                let c = client
-                    .characteristic_by_uuid::<str>(&service, &Uuid::new_short(0x2B97))
+    // Extremely weird fix: for some reason, trouble doesn't allow GATT requests at once and will
+    // timeout. Using mutex to ensure these two futures are mutually exclusive
+
+    let one_only: Mutex<CriticalSectionRawMutex, ()> = Mutex::new(());
+
+    join(
+        async {
+            loop {
+                let lock = one_only.lock().await;
+
+                let track_title_chr = client
+                    .characteristic_by_uuid::<str>(&service, &TRACK_TITLE.into())
                     .await
                     .unwrap();
 
                 let mut data = [0u8; 64];
-                let amnt = client.read_characteristic(&c, &mut data).await.unwrap();
-
-                info!("{:?}", str::from_utf8(&data[..amnt]).unwrap());
-                info!("{:?}", Debug2Format(&c));
-            }
-            "play" => {
-                let c = client
-                    .characteristic_by_uuid::<u8>(&service, &MEDIA_CONTROL_POINT.into())
+                let amnt = client
+                    .read_characteristic(&track_title_chr, &mut data)
                     .await
                     .unwrap();
 
-                client.write_characteristic(&c, &[0x01]).await.unwrap();
-            }
-            "pause" => {
-                let c = client
-                    .characteristic_by_uuid::<u8>(&service, &MEDIA_CONTROL_POINT.into())
+                let track_title = core::str::from_utf8(&data[..amnt]).unwrap_or("");
+
+                info!("Track title: {}", track_title);
+
+                let track_duration_chr = client
+                    .characteristic_by_uuid::<i32>(&service, &TRACK_DURATION.into())
+                    .await
+                    .unwrap();
+                let mut data = [0u8; 64];
+                let _ = client
+                    .read_characteristic(&track_duration_chr, &mut data)
                     .await
                     .unwrap();
 
-                client.write_characteristic(&c, &[0x02]).await.unwrap();
+                // TODO: Maybe check?
+                let track_duration =
+                    i32::from_le_bytes(data[..4].try_into().unwrap()) as f32 * 0.01;
+
+                let track_position_chr = client
+                    .characteristic_by_uuid::<i32>(&service, &TRACK_POSITION.into())
+                    .await
+                    .unwrap();
+                let mut data = [0u8; 64];
+                let _ = client
+                    .read_characteristic(&track_position_chr, &mut data)
+                    .await
+                    .unwrap();
+
+                let mut data = [0u8; 64];
+                client
+                    .read_characteristic_by_uuid(&service, &MEDIA_STATE.into(), &mut data)
+                    .await
+                    .unwrap();
+                let playing = *data.first().unwrap_or(&0u8) == 0x01u8;
+
+                // TODO: Maybe check?
+                let track_position =
+                    i32::from_le_bytes(data[..4].try_into().unwrap()) as f32 * 0.01;
+                info!("Track pos: {}", track_position);
+
+                draw_screen(
+                    track_title,
+                    track_position / track_duration,
+                    playing,
+                    display,
+                )
+                .await;
+
+                drop(lock);
+
+                Timer::after_secs(1).await;
             }
-            "" => {}
-            _ => info!("Don't know what that is!"),
-        }
-    }
+        },
+        async {
+            loop {
+                if button_matrix.0.is_low() {
+                    info!("left");
+
+                    let _lock = one_only.lock().await;
+                    client
+                        .write_characteristic(&mcp_chr, &[0x30])
+                        .await
+                        .unwrap();
+                }
+                if button_matrix.1.is_low() {
+                    info!("mid");
+
+                    let _lock = one_only.lock().await;
+                    let mut buf = [0u8; 8];
+                    client
+                        .read_characteristic_by_uuid(&service, &MEDIA_STATE.into(), &mut buf)
+                        .await
+                        .unwrap();
+
+                    // Playing
+                    let opcode = if *buf.first().unwrap_or(&0u8) == 0x01u8 {
+                        0x02
+                    } else {
+                        0x01
+                    };
+
+                    client
+                        .write_characteristic(&mcp_chr, &[opcode])
+                        .await
+                        .unwrap();
+                }
+                if button_matrix.2.is_low() {
+                    info!("right");
+
+                    let _lock = one_only.lock().await;
+                    client
+                        .write_characteristic(&mcp_chr, &[0x31])
+                        .await
+                        .unwrap();
+                }
+
+                Timer::after_millis(20).await;
+            }
+        },
+    )
+    .await;
 }
 
 async fn advertise<'values, 'server, C: Controller>(
@@ -252,11 +433,12 @@ async fn advertise<'values, 'server, C: Controller>(
     let len = AdStructure::encode_slice(
         &[
             AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
-            AdStructure::CompleteLocalName(b"TrouBLE"),
+            AdStructure::CompleteLocalName(b"Widget"),
         ],
         &mut adv_data,
     )
     .unwrap();
+
     let advertiser = peripheral
         .advertise(
             &Default::default(),
@@ -292,22 +474,7 @@ async fn main(_spawner: Spawner) -> () {
     // generator parameters: --chip esp32s3 -o esp32s3-wroom-2 -o unstable-hal -o alloc -o wifi -o embassy -o ble-trouble -o probe-rs -o defmt -o panic-rtt-target -o wokwi -o vscode
 
     // Configure RTT channels
-    let channels = rtt_target::rtt_init! {
-        up: {
-            0: { size: 256,  mode: ChannelMode::NoBlockSkip, name: "terminal", section: ".rtt_data" }
-            1: { size: 1024, mode: ChannelMode::NoBlockSkip, name: "defmt", section: ".rtt_data" }
-            2: { size: 256,  mode: ChannelMode::NoBlockSkip, name: "binary", section: ".rtt_data" }
-        }
-        down: {
-            0: { size: 16, name: "terminal", section: ".rtt_data" }
-        }
-        section_cb: ".rtt_data"
-    };
-
-    let mut input = channels.down.0;
-
-    set_print_channel(channels.up.0);
-    set_defmt_channel(channels.up.1);
+    rtt_init_defmt!();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
@@ -360,6 +527,18 @@ async fn main(_spawner: Spawner) -> () {
     let storage_range = (capacity - erase_size * 2)..capacity;
     let flash = embassy_embedded_hal::adapter::BlockingAsync::new(flash_storage);
 
+    // Storage logic
+    let mut map_storage =
+        MapStorage::<StoredAddr, _, _>::new(flash, MapConfig::new(storage_range), NoCache::new());
+
+    info!("Storage Initialized");
+
+    // GPIO Buttons
+    let config = InputConfig::default().with_pull(Pull::Up);
+    let button_left = Input::new(peripherals.GPIO16, config);
+    let button_mid = Input::new(peripherals.GPIO17, config);
+    let button_right = Input::new(peripherals.GPIO18, config);
+
     // I2C Bus
     let i2c = I2c::new(
         peripherals.I2C0,
@@ -380,10 +559,6 @@ async fn main(_spawner: Spawner) -> () {
     .into_buffered_graphics_mode();
     display.init().await.unwrap();
     info!("Initalized display");
-
-    // Storage logic
-    let mut map_storage =
-        MapStorage::<StoredAddr, _, _>::new(flash, MapConfig::new(storage_range), NoCache::new());
 
     // Check if bond info there
     let mut buf = [0u8; 64];
@@ -407,10 +582,7 @@ async fn main(_spawner: Spawner) -> () {
                 ltk: value.ltk,
             })
             .unwrap();
-        true
-    } else {
-        false
-    };
+    }
 
     stack.set_io_capabilities(IoCapabilities::NoInputNoOutput);
     let host = stack.build();
@@ -419,7 +591,7 @@ async fn main(_spawner: Spawner) -> () {
 
     let server = Server::new_with_config(GapConfig::Peripheral(PeripheralConfig {
         name: "Widget",
-        appearance: &appearance::REMOTE_CONTROL,
+        appearance: &appearance::TAG,
     }))
     .unwrap();
 
@@ -436,56 +608,12 @@ async fn main(_spawner: Spawner) -> () {
 
     display.flush().await.unwrap();
 
-    loop {
-        display.clear(BinaryColor::Off).unwrap();
-
-        Text::with_alignment(
-            "Song Title",
-            Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4),
-            style,
-            Alignment::Center,
-        )
-        .draw(&mut display)
-        .unwrap();
-
-        // TODO: Rounded rectangles
-        let percentage = 0.3;
-        let width = (SCREEN_WIDTH as f32 * 0.8) as i32;
-        let done = (percentage * width as f32) as i32;
-        let margin = (SCREEN_WIDTH as f32 * 0.8) as i32;
-
-        Rectangle::new(Point::new(margin, SCREEN_HEIGHT / 2), Size::new(done, 10));
-
-        Image::with_center(
-            &PLAY_IMG,
-            Point::new(SCREEN_WIDTH / 2, 3 * SCREEN_HEIGHT / 4),
-        )
-        .draw(&mut display)
-        .unwrap();
-
-        Image::with_center(
-            &BACKWARD_IMG,
-            Point::new(SCREEN_WIDTH / 4, 3 * SCREEN_HEIGHT / 4),
-        )
-        .draw(&mut display)
-        .unwrap();
-
-        Image::with_center(
-            &FORWARD_IMG,
-            Point::new(3 * SCREEN_WIDTH / 4, 3 * SCREEN_HEIGHT / 4),
-        )
-        .draw(&mut display)
-        .unwrap();
-
-        display.flush().await.unwrap();
-    }
-
-    let conn = advertise(&mut peripheral, &server).await;
-
     info!("Starting!");
 
     let _ = join(host_runner_task(&mut runner), async {
         loop {
+            let conn = advertise(&mut peripheral, &server).await;
+
             let client =
                 GattClient::<ExternalController<_, 1>, DefaultPacketPool, 32>::new_no_mtu_bs(
                     &stack,
@@ -534,12 +662,18 @@ async fn main(_spawner: Spawner) -> () {
                 join(gatt_client_task(&client), async {
                     loop {
                         if conn.raw().security_level().unwrap().encrypted() {
+                            info!("Session now encrypted!");
                             break;
                         }
                         Timer::after_secs(2).await;
                     }
 
-                    app_logic(&mut input, &client, &mut display).await;
+                    app_logic(
+                        (&button_left, &button_mid, &button_right),
+                        &client,
+                        &mut display,
+                    )
+                    .await;
                 }),
             )
             .await;
